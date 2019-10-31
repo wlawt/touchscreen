0594e51c93235de95b1906dfe757e93b20f222d9
Will L <horizon.william14@gmail.com>
2019-10-30 20:09:43 -0400

    added routes

---
+++ routes/api/aws.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+/*  @route      POST api/aws
+    @desc       Send req to AWS server
+    @access     Public
+*/
+router.post("/", (req, res) => {
+  // Do stuff here.
+});
+
+module.exports = router;
